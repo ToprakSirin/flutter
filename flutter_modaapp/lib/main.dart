85d1337c9a6f468999cec3aaedeb247f82bc7243
ToprakSirin <sirintprk21@gmail.com>
2021-07-09 10:41:49 +0300

    ModaApp (Koda dökülecek Tasarım Ve Bölüm 1)

---
+++ flutter_modaapp/lib/main.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(ModaApp());
+}
+
+class ModaApp extends StatelessWidget {
+  const ModaApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Anasayfa(),
+    );
+  }
+}
+
+class Anasayfa extends StatefulWidget {
+  Anasayfa({Key? key}) : super(key: key);
+
+  @override
+  _AnasayfaState createState() => _AnasayfaState();
+}
+
+class _AnasayfaState extends State<Anasayfa> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        title: Text(
+          "Moda Uygulaması",
+          style: TextStyle(
+              fontFamily: 'Montserrat',
+              fontSize: 22,
+              color: Colors.black,
+              fontWeight: FontWeight.bold),
+        ),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.camera_alt),
+            onPressed: () {},
+            color: Colors.grey,
+          ),
+        ],
+      ),
+      body: ListView(
+        padding: EdgeInsets.only(top: 10),
+        children: [
+          Container(
+            color: Colors.blue.shade300,
+            height: 150,
+            width: double.infinity,
+            child: ListView(
+              padding: EdgeInsets.all(10),
+              scrollDirection: Axis.horizontal,
+              children: [
+                listeElemani('assets/images/model1.jpeg',
+                    'assets/images/chanellogo.jpg'),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget listeElemani(String imagePath, String logoPath) {
+    return Column(
+      children: [
+        Stack(
+          children: [
+            Container(
+              height: 75,
+              width: 75,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(30),
+                image: DecorationImage(
+                  image: AssetImage(imagePath),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
