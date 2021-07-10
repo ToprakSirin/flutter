cf823e0fef22f0cbe7ac11a0b81eca5bbc7a5dab
ToprakSirin <sirintprk21@gmail.com>
2021-07-10 13:42:10 +0300

    Genel tekrar

--- flutter_json_http/lib/main.dart
+++ flutter_json_http/lib/main.dart
@@ -6,16 +6,74 @@ void main() {
   runApp(MyApp());
 }
 
+class Ogrenci {
+  int id;
+  String isim;
+
+  Ogrenci(this.id, this.isim);
+  @override
+  String toString() {
+    return "Adı: $isim id:$id";
+  }
+
+  factory Ogrenci.mapNesneyeDonustur(Map<String, dynamic> gelenMap) {
+    return Ogrenci(gelenMap['id'], gelenMap['isim']);
+  }
+  Ogrenci.fromMap(Map<String, dynamic> gelenMap)
+      : id = gelenMap['id'],
+        isim = gelenMap['isim'];
+}
+
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    Ogrenci emre = Ogrenci(10, "emre");
+    debugPrint(emre.toString());
+
+    Map<String, dynamic> hasanMaP = {'id': 15, 'isim': "Hasan"};
+    debugPrint("Adı :" + hasanMaP['isim'] + "id:" + hasanMaP['id'].toString());
+    Ogrenci yeni = Ogrenci.mapNesneyeDonustur(hasanMaP);
+    debugPrint(yeni.toString());
+
+    Map<String, dynamic> fatmaMaP = {'id': 20, 'isim': "Fatma"};
+    Ogrenci fatma = Ogrenci.fromMap(fatmaMaP);
+    debugPrint(fatma.toString());
+
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: LocalJsonKullanimi(),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Json ve Api"),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            ElevatedButton(
+              child: Text("Local Json"),
+              style: ElevatedButton.styleFrom(primary: Colors.green),
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) => LocalJsonKullanimi(),
+                  ),
+                );
+              },
+            )
+          ],
+        ),
+      ),
     );
   }
 }
