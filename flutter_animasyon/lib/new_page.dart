516bad9b5fa6cd8d31bdc511a14f6842fd0cfab8
ToprakSirin <sirintprk21@gmail.com>
2021-07-26 17:53:22 +0300

    .AnimatedBuilder ve Sıralı - Staggared Animasyon Örnek

--- flutter_animasyon/lib/new_page.dart
+++ flutter_animasyon/lib/new_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_sequence_animation/flutter_sequence_animation.dart';
 
 class NewPage extends StatefulWidget {
   NewPage({Key? key}) : super(key: key);
@@ -7,11 +8,81 @@ class NewPage extends StatefulWidget {
   _NewPageState createState() => _NewPageState();
 }
 
-class _NewPageState extends State<NewPage> {
+class _NewPageState extends State<NewPage> with SingleTickerProviderStateMixin {
+  AnimationController? controller;
+  SequenceAnimation? sequenceAnimation;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    controller = AnimationController(vsync: this);
+
+    sequenceAnimation = SequenceAnimationBuilder()
+        .addAnimatable(
+            animatable: Tween<double>(begin: 0, end: 1),
+            from: Duration.zero,
+            to: Duration(milliseconds: 200),
+            tag: "opacity")
+        .addAnimatable(
+            animatable: Tween<double>(begin: 50, end: 150),
+            from: Duration(milliseconds: 250),
+            to: Duration(milliseconds: 500),
+            tag: "width")
+        .addAnimatable(
+            animatable: Tween<double>(begin: 50, end: 150),
+            from: Duration(milliseconds: 500),
+            to: Duration(milliseconds: 750),
+            tag: "height")
+        .addAnimatable(
+            animatable: EdgeInsetsGeometryTween(
+              begin: EdgeInsets.only(bottom: 16),
+              end: EdgeInsets.only(bottom: 75),
+            ),
+            from: Duration(milliseconds: 750),
+            to: Duration(milliseconds: 1000),
+            curve: Curves.easeIn,
+            tag: "padding")
+        .addAnimatable(
+            animatable: new BorderRadiusTween(
+              begin: new BorderRadius.circular(4.0),
+              end: new BorderRadius.circular(75.0),
+            ),
+            from: Duration(milliseconds: 750),
+            to: Duration(milliseconds: 1000),
+            curve: Curves.ease,
+            tag: "border")
+        .animate(controller!); //opacity için
+
+    controller!.forward();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-       child: null,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Next Page"),
+      ),
+      body: Center(
+        child: AnimatedBuilder(
+          animation: controller!,
+          builder: (BuildContext context, Widget? child) {
+            return Container(
+              padding: sequenceAnimation!["padding"].value,
+              child: Opacity(
+                opacity: sequenceAnimation!["opacity"].value,
+                child: Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(width: 3, color: Colors.orange),
+                  ),
+                  width: sequenceAnimation!["width"].value,
+                  height: sequenceAnimation!["height"].value,
+                ),
+              ),
+            );
+          },
+        ),
+      ),
     );
   }
 }
