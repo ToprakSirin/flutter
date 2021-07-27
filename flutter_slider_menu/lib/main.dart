f0b7358ac5d74f98510d7fb34dc4e41b276bcf66
ToprakSirin <sirintprk21@gmail.com>
2021-07-27 14:11:27 +0300

    Animasyonlu SliderMenu

---
+++ flutter_slider_menu/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import 'menu_dashboard_widget.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Menu Dashboard',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MenuDashboard(),
+    );
+  }
+}
