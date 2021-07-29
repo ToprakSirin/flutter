ed7f975947439548db4dcb1e87a7f0502a4e4fd8
ToprakSirin <sirintprk21@gmail.com>
2021-07-29 11:16:30 +0300

    Flutter_bloc Plugini

---
+++ flutter_bloc_paket/lib/bloc_plugin/cubit/sayici_state.dart
@@ -0,0 +1,8 @@
+part of 'sayici_cubit.dart';
+
+class SayiciState {
+  int _deger;
+  SayiciState(this._deger);
+
+  int get deger => _deger;
+}
--- flutter_bloc_paket/lib/theme_cubit.dart
+++ flutter_bloc_paket/lib/theme_cubit.dart
@@ -7,12 +7,17 @@ class ThemeCubit extends Cubit<ThemeData> {
   static final _acikTema = ThemeData(
       floatingActionButtonTheme: FloatingActionButtonThemeData(
         foregroundColor: Colors.white,
+        backgroundColor: Colors.teal,
       ),
+      primarySwatch: Colors.purple,
+      visualDensity: VisualDensity.adaptivePlatformDensity,
       brightness: Brightness.light);
   static final _koyuTema = ThemeData(
-      floatingActionButtonTheme:
-          FloatingActionButtonThemeData(foregroundColor: Colors.orange),
-      brightness: Brightness.dark);
+    floatingActionButtonTheme: FloatingActionButtonThemeData(
+        foregroundColor: Colors.orange, backgroundColor: Colors.yellow),
+    brightness: Brightness.dark,
+    primarySwatch: Colors.purple,
+  );
 
   void temaDegistir() {
     emit(state.brightness == Brightness.dark ? _acikTema : _koyuTema);
