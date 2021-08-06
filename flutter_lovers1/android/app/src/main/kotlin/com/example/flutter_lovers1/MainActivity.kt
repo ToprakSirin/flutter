c1df6c590805576d1d38b19f1f62a35bc8eccccb
ToprakSirin <sirintprk21@gmail.com>
2021-08-06 17:25:18 +0300

    yok

--- flutter_lovers1/android/app/src/main/kotlin/com/example/flutter_lovers1/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.flutter_lovers1
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- flutter_lovers1/lib/services/storage_base.dart
+++
@@ -1,6 +0,0 @@
-import 'package:image_picker/image_picker.dart';
-
-abstract class StorageBase {
-  Future<String> uploadFile(
-      String userId, String fileType, XFile yuklenecekDosya);
-}
